ec5bc31ccc3a5ef1a61475c05d0c7332b7544f08
1bucket <penrade43@gmail.com>
2025-10-26 15:20:09 -0400

    created custom form element, added title field

---
+++ components/eventcreatorform.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { z } from 'zod';
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+    title: z.string().min(1).max(50),
+    description: z.string().min(0).max(1000),
+    dates: z.array(z.iso.date()),
+    timestart: z.iso.time(),
+    timeend: z.iso.time()
+});
+
+export function EventCreatorForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: "Untitled",
+            description: "",
+            dates: [new Date().toISOString().slice(0, 10)],
+            timestart: "",
+            timeend: ""
+        },
+    });
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // only called when schema matches
+        console.log(values);
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Event Title</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Untitled" {...field} />
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
