9158e9b09e9b27d63128b71aecf8331161e7b3e7
1bucket <penrade43@gmail.com>
2025-10-26 15:18:07 -0400

    created eventcreator page

---
+++ app/eventcreator/page.tsx
@@ -0,0 +1,11 @@
+// import { EventCreatorForm } from '@/components/eventcreatorform'
+// import { DateSelector } from '@/components/dateselector'
+
+export default function EventCreator() {
+    return (
+        <div className="">
+            {/* <EventCreatorForm/>
+            <DateSelector/> */}
+        </div>
+    );
+}
