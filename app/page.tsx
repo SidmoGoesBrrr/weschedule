6cb3779881c607d082d6ec2fcdbe24054ca65c67
Zain Raza <145010320+Sizzle730@users.noreply.github.com>
2025-10-31 17:26:26 +0000

    added componens for hero page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import SonnerDemo from "@/components/mysonner"
 import AlertDialogDemo from "@/components/myalertdialog"
+import MarqueeDemo from "@/components/mymarquee";
+import InputDemo from "@/components/myinput";
+import MenubarDemo from "@/components/mymenubar";
+import NavigationMenuDemo from "@/components/mynavigation_menu";
+
 export default function Home() {
   return (
    
     <div>
-      <h1>Hello World</h1>
+      <h1>Welcome to WeSchedule</h1>
       <SonnerDemo />
       <AlertDialogDemo />
     </div>
