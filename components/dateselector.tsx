15431607a2b7bc3f588bf9888e0eb21d7b55395b
1bucket <penrade43@gmail.com>
2025-11-21 16:17:05 -0500

    deleted some code

--- components/dateselector.tsx
+++ components/dateselector.tsx
@@ -4,16 +4,6 @@ import { Calendar } from "@/components/ui/calendar";
 import { useState, useEffect } from 'react';
 
 export function DateSelector(props) {
-    // const [dates, setDates] = useState([]);
-    // let dates = [];
-    const update = (newDates) => {
-        // setDates(dates);
-        // console.log(newDates);
-        props.updateFormCallback(newDates);
-        // console.log("upd", dates);
-    }
-
-    // useEffect(() => console.log("calendar's dates", props.dates));
     
     return (
         <Calendar
