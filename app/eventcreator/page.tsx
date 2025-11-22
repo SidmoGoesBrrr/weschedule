5b4247eceae72b7d93c26a752d15c43d782029df
1bucket <penrade43@gmail.com>
2025-11-22 11:05:09 -0500

    fixed daypicker appearance and added padding to eventcreator page

--- app/eventcreator/page.tsx
+++ app/eventcreator/page.tsx
@@ -4,7 +4,7 @@ import { EventCreatorForm } from '@/components/eventcreatorform'
 
 export default function EventCreator() {
     return (
-        <div className="">
+        <div className="pt-15 pl-30 pb-15 pr-30">
             <EventCreatorForm/>
         </div>
     );
