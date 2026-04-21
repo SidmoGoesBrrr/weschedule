b73e8501697f8e5a4e71381c738ee2b5789e922a
1bucket <penrade43@gmail.com>
2026-04-21 19:50:16 -0400

    removed .env and .next/ from tracking

--- .next/dev/static/chunks/app_page_tsx_75ef09a5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_1e0ddd1f._.js",
-  "static/chunks/_fe395ed5._.js"
-],
-    source: "dynamic"
-});
